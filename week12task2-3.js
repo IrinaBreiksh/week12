02a14415dfb2969967d169534f59c4ab85d8057e
Irina <irinabreiksh@gmail.com>
2023-11-03 20:30:01 +0300

    практикум 12, задание 2(текст) и 3(калькулятор ИМТ)

---
+++ week12task2-3.js
@@ -0,0 +1,56 @@
+
+// Задание 2
+
+const text = 'у лукоморья клён зелёный; златая цепь на клён том; и днём и ночью кот учёный; всё ходит по цепи кругом; идёт направо — песнь заводит; налево — сказку говорит; там чудеса: там леший бродит; русалка на ветвях сидит; там на неведомых дорожках; следы невиданных зверей; избушка там на курьих ножках; стоит без окон, без дверей; там лес и дол видений полны; там о заре прихлынут волны; на брег песчаный и пустой; и тридцать витязей прекрасных; чредой из вод выходят ясных; и с ними дядька их морской; там королевич мимоходом; пленяет грозного царя; там в облаках перед народом; через леса, через моря; колдун несёт богатыря; в темнице там царевна тужит; а бурый волк ей верно служит; там ступа с бабою ягой; идёт, бредёт сама собой; там царь кащей над златом чахнет; там русский дух… там русью пахнет!; и там я был, и мёд я пил; у моря видел клён зелёный; под ним сидел, и кот учёный; свои мне сказки говорил.'
+
+console.log(text.length);
+
+const result = text.replace(/;/g, '\n');
+
+const substring = text.replaceAll(' ','');
+
+const newText = text.slice(28,50);
+
+const replacedText = newText.replace('клён', 'дубе');
+
+const replacedText2 = result.toUpperCase();
+
+const replacedText3 = result.replaceAll(/клён/gi, 'дуб');
+
+const index = text.search('моря');
+
+const modifiedText = replacedText3.toUpperCase().slice(0,1) + replacedText3.toLowerCase().slice(1);
+
+console.log(`1. Количество символов в тексте: ${text.length}`);
+console.log(`2. Текст с переносами строк: \n ${result}`);
+console.log(`3. Текст с удалёнными пробелами: ${substring}`);
+console.log(`4. Извлечение подстроки: ${newText}`);
+console.log(`5. Замена "клён" на "дубе": ${replacedText}`);
+console.log(`6. Строка result в верхнем регистре: \n ${replacedText2}`);
+console.log(`7. Замена "клён" на "дуб" (все вхождения) в переменной result: \n ${replacedText3}`);
+console.log(`8. Индекс первого вхождения "моря": ${index}`);
+console.log(`9. Строка с измененной первой буквой: \n ${modifiedText}`);
+
+
+// Задание 3
+
+// ИМТ = вес (кг) / (рост (м) * рост (м))
+
+const buttonCalc = document.getElementById('button');
+
+function calculateBMI(){
+    const weight = document.querySelector('#weight').value;
+    const height = document.querySelector('#height').value;
+
+    const heightInMetres = height/100;
+
+    const result = Math.round(weight/Math.pow(heightInMetres, 2));
+
+    const resultCalculated = document.querySelector('#calc-result');
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = `Ваш индекс массы тела равен ${result}`;
+    
+    resultCalculated.append(newParagraph);
+}
+
+button.addEventListener('click', calculateBMI);
